c13b6e1ea9413dd78eafa548d5ec388350d927ed
Kamil Chrościcki <k.chroscicki@stud.elka.pw.edu.pl>
2020-12-27 15:34:21 +0100

    Commit

--- MOPS/Packet.cs
+++ MOPS/Packet.cs
@@ -6,5 +6,14 @@ namespace MOPS
 {
     class Packet
     {
+        protected double ArrivalTime { get; set; }
+        protected double ServiceTime { get; set; }
+        protected double arrivalTime;
+        protected double serviceTime;
+        public Packet()
+		{
+            arrivalTime = ArrivalTime;
+            serviceTime = ServiceTime;
+		}
     }
 }
